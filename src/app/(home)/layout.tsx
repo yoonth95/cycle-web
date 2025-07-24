328a52591d176248e01a7ac396cc73e70a9d48c7
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-07-24 22:39:01 +0900

    Feat: 홈 레이아웃 컴포넌트 추가 및 NAVER Maps API 스크립트 로드 기능 구현

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,13 @@
+import Script from "next/script";
+
+export default function HomeLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+      <Script
+        src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_API_KEY_ID}&submodules=geocoder`}
+        strategy="afterInteractive"
+      />
+    </>
+  );
+}
