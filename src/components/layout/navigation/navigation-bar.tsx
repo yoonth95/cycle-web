ced371ca2917a2fdb0ee1ca6239fbc1f4c0d269a
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-07-27 12:52:24 +0900

    Feat: 메뉴 데이터가 없을 경우에 대한 안전한 처리 추가 및 네비게이션 바의 클래스 정리

--- src/components/layout/navigation/navigation-bar.tsx
+++ src/components/layout/navigation/navigation-bar.tsx
@@ -10,7 +10,7 @@ import { NavigationItem } from "@/types/navigation-data";
 
 const NavigationBar = ({ menuData }: NavigationProps) => {
   const router = useRouter();
-  const sortedMenuData = sortMenuData(menuData);
+  const sortedMenuData = sortMenuData(menuData || []);
 
   const handleMove = (item: NavigationItem) => {
     if (item.type === "single") router.push(item.link);
@@ -18,20 +18,21 @@ const NavigationBar = ({ menuData }: NavigationProps) => {
 
   return (
     <nav>
-      <ul className="hidden lg:flex items-center">
-        {sortedMenuData.map((item) => (
-          <motion.li
-            key={item.id}
-            className={cn("group", "px-4 py-5", "duration-200 font-medium cursor-pointer")}
-            onClick={() => handleMove(item)}
-          >
-            <h3 className="text-white group-hover:text-white transition-colors duration-300">
-              {item.slug}
-            </h3>
-            <div className="h-[2px] w-0 bg-white group-hover:w-full transition-all duration-300 relative top-[5px]"></div>
-            <HoverDropdownMenu menuItem={item} />
-          </motion.li>
-        ))}
+      <ul className="hidden items-center lg:flex">
+        {sortedMenuData &&
+          sortedMenuData.map((item) => (
+            <motion.li
+              key={item.id}
+              className={cn("group", "px-4 py-5", "cursor-pointer font-medium duration-200")}
+              onClick={() => handleMove(item)}
+            >
+              <h3 className="text-white transition-colors duration-300 group-hover:text-white">
+                {item.slug}
+              </h3>
+              <div className="relative top-[5px] h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></div>
+              <HoverDropdownMenu menuItem={item} />
+            </motion.li>
+          ))}
       </ul>
     </nav>
   );
