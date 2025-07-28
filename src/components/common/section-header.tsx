262daeb1ff9638790e074c544b6239b5d943182f
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-07-29 00:03:18 +0900

    Feat: SectionHeader 설명 글 break-keep 추가

--- src/components/common/section-header.tsx
+++ src/components/common/section-header.tsx
@@ -6,9 +6,9 @@ const SectionHeader = ({ title, description }: { title: string; description: str
   const [isMobile, _] = useMobile({ breakpoint: 640 });
 
   return (
-    <div className="text-center mx-auto mb-8 lg:mb-12">
+    <div className="mx-auto mb-8 text-center lg:mb-12">
       <h2 className={`${isMobile ? "heading-4" : "heading-3"} text-foreground mb-4`}>{title}</h2>
-      <p className={`${isMobile ? "body-small" : "body-medium"} text-muted-foreground`}>
+      <p className={`${isMobile ? "body-small" : "body-medium"} text-muted-foreground break-keep`}>
         {description}
       </p>
     </div>
