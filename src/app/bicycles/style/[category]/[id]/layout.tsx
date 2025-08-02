e8e8a1c215590497e31fe934ae39f56e4647b2e1
윤태현 <yoonth0919@gmail.com>
2025-08-02 14:17:57 +0900

    Feat: 자전거 상세 페이지 및 레이아웃 컴포넌트 추가

---
+++ src/app/bicycles/style/[category]/[id]/layout.tsx
@@ -0,0 +1,3 @@
+export default function BicycleDetailLayout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
---
+++ src/app/bicycles/style/[category]/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { DetailContent } from "@/components/features/bicycles/detail";
+
+export default function BicycleDetailPage() {
+  return <DetailContent />;
+}
