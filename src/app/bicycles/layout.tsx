f1e3d6d91b24a89098effc4ca9df5af6129833b0
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-09-02 20:40:23 +0900

    Refactor: 자전거 페이지 layout.tsx 테스트용 Link 리스트 제거

--- src/app/bicycles/layout.tsx
+++ src/app/bicycles/layout.tsx
@@ -1,20 +1,3 @@
-import Link from "next/link";
-
 export default function BicyclesLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="min-h-[calc(100vh-64px)] bg-gray-50">
-      <div className="flex flex-col gap-3">
-        <Link href="/bicycles" prefetch={true}>
-          자전거 페이지
-        </Link>
-        <Link href="/bicycles/style" prefetch={true}>
-          스타일 페이지
-        </Link>
-        <Link href="/bicycles/style/smart-mobility" prefetch={true}>
-          전기 자전거
-        </Link>
-      </div>
-      {children}
-    </main>
-  );
+  return <main className="min-h-[calc(100vh-64px)] bg-gray-50">{children}</main>;
 }
