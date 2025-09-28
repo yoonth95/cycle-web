d563ba07e47a0c9e96a7a435079bf70eb247fb7c
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-09-29 01:03:34 +0900

    Refactor: 블로그 카드 및 리스트 아이템 컴포넌트에서 이미지 URL 생성 방식 개선 및 next.config.ts에 새로운 호스트 추가 (#36)

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "tmgpqpvbjvrppwpnumcs.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "postfiles.pstatic.net",
+      },
     ],
     formats: ["image/avif", "image/webp"],
   },
