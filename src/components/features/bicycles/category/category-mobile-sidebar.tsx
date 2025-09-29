49023331f6cfd7980ad697ae4170b2013fafbb2a
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-09-29 23:16:41 +0900

    Fix: 트리거 버튼 안 나오던 버그 수정 (#23)

--- src/components/features/bicycles/category/category-mobile-sidebar.tsx
+++ src/components/features/bicycles/category/category-mobile-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Sheet,
   SheetContent,
@@ -6,6 +8,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
 import CategoryLayoutSidebar from "./category-layout-sidebar";
 import type { CategoryListItemType } from "@/types/bicycle";
 import { Funnel } from "lucide-react";
@@ -23,9 +26,9 @@ const CategoryMobileSidebar = ({
     <div className="fixed right-6 bottom-6 z-50">
       <Sheet>
         <SheetTrigger asChild>
-          <span className="hover:bg-figma-thunderbird bg-figma-alizarin-crimson relative flex cursor-pointer items-center justify-center rounded-full p-2 text-white shadow-lg transition-colors">
+          <Button size="icon">
             <Funnel className="h-5 w-5" />
-          </span>
+          </Button>
         </SheetTrigger>
 
         <SheetContent side="right" className="mt-16 w-80">
